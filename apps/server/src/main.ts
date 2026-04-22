dd23abce51deb5e2863ae51d4df9901a36387fd7
Julian Pömp <jp@poemp.net>
2026-04-22 15:36:46 +0200

    path not working if ends with separator

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -6,6 +6,7 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { access } from 'node:fs/promises';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,7 +14,23 @@ async function bootstrap() {
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3000;
   await app.listen(port);
-  Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`);
+  Logger.log(
+    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`,
+  );
+
+  console.log(`\nTEST if Directory exists`);
+  console.log(`${__dirname}: ${await pathExists(__dirname)}`);
+  console.log(`${__dirname + '/'}: ${await pathExists(__dirname + '/')}`);
+}
+
+async function pathExists(path: string) {
+  try {
+    await access(path);
+    return true;
+  } catch (e) {
+    console.log(`ERROR: ${e}`);
+    return false;
+  }
 }
 
 bootstrap();
